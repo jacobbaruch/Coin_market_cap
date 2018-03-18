a0a3c133b1bb5ce055df3865c992e6f46540afa4
Jacob Baruch <22732711+jacobbaruch@users.noreply.github.com>
2018-03-18 21:41:05 +0200

    Add files via upload

---
+++ Coins_history.py
@@ -0,0 +1,155 @@
+import json
+import sys
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from _datetime import datetime, date, timedelta
+
+
+def save_crypto_coins_history(i_rank_start=1, i_rank_end=10, i_coin_file_path='crypto_coins',
+                             i_from_date=None, i_to_date=None, i_min_volume=100000):
+ """
+ :param int i_rank_start: pull data from coin current ranking [includes]
+ :param int i_rank_end:  pull data till coin current ranking [includes]
+ :param str i_coin_file_path: target csv file name
+ :param str 'YYYY-MM-DD' i_from_date: pull data from this date [includes]
+ :param str 'YYYY-MM-DD' i_to_date: pull data till this date [includes]
+ :param int i_min_volume:  pull coins with 24 Hrs volume bigger/equal than this value
+ writes to a csv file - historic data of coins
+ """
+
+ from_date, to_date = get_from_to_dates(i_from_date, i_to_date)
+ rank_range_from_start = i_rank_end-i_rank_start+1
+ coins_ranking_dict = get_coins_current_ranking(i_rank_start, rank_range_from_start, i_min_volume)
+
+ df_coins = pd.DataFrame([])
+ for rank, coin in coins_ranking_dict.items():
+  df_coins = df_coins.append(get_coins_historical_data(rank, coin, from_date, to_date))
+  write_df_to_csv(df_coins, i_coin_file_path + '.csv')
+
+
+def get_coins_current_ranking(i_start, i_limit, i_min_volume):
+ """
+ :param int i_start: pull data from coin current ranking [includes]
+ :param int i_limit: pull data till coin current ranking [includes]
+ :param float i_min_volume: pull coins with 24 Hrs volume bigger [includes] than this value
+ :return dict: rank, coin name
+ """
+
+ url_coin_list_json = 'https://api.coinmarketcap.com/v1/ticker/?start={}&limit={}'.format(i_start - 1, i_limit)
+ page = requests.get(url_coin_list_json)
+ json_file = json.loads(page.text)
+
+ coins_dict = {}
+ for k in json_file:
+  if float(k['24h_volume_usd']) >= i_min_volume:
+   coins_dict[k['rank']] = k['id']
+
+ return coins_dict
+
+
+def get_coins_historical_data(i_rank, i_coin, i_from_date, i_to_date):
+ """
+ :param int i_rank: current coin rank
+ :param str i_coin: coin name
+ :param date 'YYYYMMDD' i_from_date: pull data from this date [includes]
+ :param date 'YYYYMMDD' i_to_date: pull data till this date [includes]
+ return list: coin history data includes current ranking
+ """
+
+ df_coin = get_specific_coin_historical_data(i_coin, i_from_date, i_to_date)
+ df_coin['Coin'] = i_coin
+ df_coin['Cur. Rank'] = i_rank
+ df_coin = pd.concat([df_coin.iloc[:,7:], df_coin.iloc[:,0:7]], axis=1, join_axes=[df_coin.index])
+
+ return df_coin
+
+
+def  get_specific_coin_historical_data(i_coin, i_from_date, i_to_date):
+ """
+ :param str i_coin: coin name
+ :param date 'YYYYMMDD' i_from_date: pull data from this date [includes]
+ :param date 'YYYYMMDD' i_to_date: pull data till this date [includes]
+ return list: coin history data
+ """
+
+ currencies = "https://coinmarketcap.com/currencies/"
+ currencies_end = '/historical-data/'
+ dates = '?start={}&end={}'.format(i_from_date, i_to_date)
+
+ # collect and parse coin historical page
+ url = currencies + i_coin + currencies_end + dates
+ page = requests.get(url)
+ soup = BeautifulSoup(page.text, 'html.parser')
+
+ # Pull and append historic data
+ table = soup.find('table')
+ data = {
+  'Date': [],
+  'Open': [],
+  'High': [],
+  'Low': [],
+  'Close': [],
+  'Volume': [],
+  'Market Cap': []
+ }
+
+ try:
+  rows = table.findAll('tr')[1:]
+  for row in rows:
+   cols = row.findAll('td')
+   data['Date'].append(cols[0].string)
+   data['Open'].append(cols[1].string)
+   data['High'].append(cols[2].string)
+   data['Low'].append(cols[3].string)
+   data['Close'].append(cols[4].string)
+   data['Volume'].append(cols[5].string)
+   data['Market Cap'].append(cols[6].string)
+
+  coin_data = pd.DataFrame(data)
+
+ except AttributeError as e:
+  print('input parameters not valid')
+  sys.exit(13)
+
+ return coin_data
+
+
+def write_df_to_csv(i_df, i_file):
+ """
+ :param dataframe i_df: dataframe to save
+ :param str i_file: output csv file name
+ :exception IOerror: trying to save the file
+ """
+
+ try:
+  i_df.to_csv(i_file)
+ except IOError as e:
+  print(e)
+  sys.exit(13)
+
+
+def get_from_to_dates(i_from_date, i_to_date):
+ """
+ :param str 'YYYY-MM-DD' i_from_date: pull data from this date [includes]
+ :param str 'YYYY-MM-DD' i_to_date: pull data till this date [includes]
+ :exception ValueError: date format is not as asked
+ :return tuple: dates in format 'YYYYMMDD' - dates ready to be scrapped
+ """
+ try:
+  if i_from_date is None:
+   from_date = str(date.today() + timedelta(days=-30))
+  else:
+   from_date = i_from_date
+  from_date = datetime.strptime(from_date, '%Y-%m-%d').strftime('%Y%m%d')
+
+  if i_to_date is None:
+   to_date = str(date.today() + timedelta(days=-1))
+  else:
+   to_date = i_to_date
+  to_date = datetime.strptime(to_date, '%Y-%m-%d').strftime('%Y%m%d')
+
+  return from_date, to_date
+ except ValueError as e:
+  print(e)
+  sys.exit(13)
---
+++ Coins_statistics.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import sys
+
+
+def save_coins_statistics(i_coins_file='crypto_coins', i_out_file='crypto_coins_stats'
+                          , i_precision = 3):
+ '''
+ :param str i_coins_file: crypto coins history file name
+ :param str i_out_file: output csv file name
+ :param int i_precision: precision point to the statistics fields
+ writes to a csv file - adding coins basic statistics to the history file
+ '''
+
+ pd.options.mode.use_inf_as_na = True
+ coins_file = i_coins_file + '.csv'
+ coins_stats_file = i_out_file + '.csv'
+ pd.set_option('precision', i_precision)
+
+ df = read_coins_csv(coins_file)
+ df = extend_daily_data(df)
+ df_extended = attach_yesterday_data(df)
+ df_extended = calc_yesterday_statistics(df_extended)
+ write_extended_df_to_csv(df_extended, coins_stats_file)
+
+
+def extend_daily_data(i_df):
+ """
+ :param dataframe i_df: Coins historic data
+ :return dataframe: adds daily changes, average and rank
+ """
+
+ # Daily changes
+ i_df['Day Change [open to high]'] = (i_df['High'] - i_df['Open']) / i_df['Open']
+ i_df['Day Change [open to close]'] = (i_df['Close'] - i_df['Open']) / i_df['Open']
+ i_df['Date'] = pd.to_datetime(i_df['Date'])
+ i_df['Market Cap'] = pd.to_numeric(i_df['Market Cap'].str.replace(',', ''))
+ i_df['Volume'] = pd.to_numeric(i_df['Volume'].str.replace(',', ''))
+
+ i_df = pd.merge(i_df, get_mean(i_df), how='left', on=['Coin'])
+ i_df = get_ranks_per_date(i_df)
+
+ return i_df
+
+
+def get_mean(i_df):
+ """
+ :param dataframe i_df: contains coins data
+ :return dataframe: contains Volume / High Averages
+ """
+
+ df_group_by_coin = (i_df.groupby(['Coin'], as_index=False)
+  .agg({'High': 'mean', 'Volume': 'mean'})
+  .rename(columns={'High': 'High Avg', 'Volume': 'Volume Avg.'}))
+ print(df_group_by_coin)
+ return df_group_by_coin
+
+
+def get_ranks_per_date(i_df):
+ """
+ :param dataframe i_df: contains coins data
+ :return dataframe: contains daily ranking of volume and market cap for each coin
+ """
+
+ i_df['Daily rank Market Cap'] = i_df.groupby('Date')['Market Cap'].rank(method='dense',ascending=False)
+ i_df['Daily rank Volume'] = i_df.groupby('Date')['Volume'].rank(method='dense',ascending=False)
+
+ return i_df
+
+
+def attach_yesterday_data(i_df):
+ """
+ :param dataframe i_df: contains coins data
+ :return dataframe: attach data of yesterday date for each date
+ """
+
+ # prepare yesterday date key
+ i_df['Yesterday'] = i_df['Date'].apply(pd.DateOffset(days=-1))
+
+ # assign data frame that will merge into 'i_df'
+ df_yesterday = i_df[['Coin', 'Date', 'High', 'Market Cap', 'Volume']]
+ df_yesterday = df_yesterday.rename(index=str, columns={'Date': 'Yesterday',
+                                                        'High': 'High Yesterday',
+                                                        'Market Cap': 'Market Cap yesterday',
+                                                        'Volume': 'Volume yesterday'})
+ # merging the data frames based on Coin + Yesterday Date
+ df_extended = pd.merge(i_df, df_yesterday, how='left', on=['Coin', 'Yesterday'])
+ df_extended.drop(['Yesterday'], axis=1, inplace=True)
+ df_extended.fillna(0, inplace=True)
+
+ return df_extended
+
+
+def calc_yesterday_statistics(i_df):
+ """
+ :param dataframe i_df: contains extended data
+ :return dataframe: for each date we calc the changes in:
+  High, Market Cap, Volume, correlation between changes
+ """
+
+ i_df['Change [High]'] = (i_df['High'] - i_df['High Yesterday']) / i_df['High Yesterday']
+ i_df['Change [Market Cap]'] = (i_df['Market Cap'] - i_df['Market Cap yesterday']) / i_df['Market Cap yesterday']
+ i_df['Change [Volume]'] = (i_df['Volume'] - i_df['Volume yesterday']) / i_df['Volume yesterday']
+
+ i_df.drop(['High Yesterday'], axis=1, inplace=True)
+ i_df.drop(['Market Cap yesterday'], axis=1, inplace=True)
+ i_df.drop(['Volume yesterday'], axis=1, inplace=True)
+
+ return i_df
+
+
+def read_coins_csv(i_file):
+ try:
+  df = pd.read_csv(i_file, index_col=0)
+ except IOError as e:
+  print(e)
+  sys.exit(13)
+
+ return df
+
+
+def write_extended_df_to_csv(i_df, i_file):
+ try:
+  i_df.to_csv(i_file, float_format='%.3f')
+ except IOError as e:
+  print(e)
+  sys.exit(13)
